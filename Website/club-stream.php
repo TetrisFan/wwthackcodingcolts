43ff4ab0caeebf58a69f30a4b38475e35a6b91ff
Vivian Lacson <vivilacson@gmail.com>
2019-08-03 16:25:30 -0500

    fixed slashes

--- Website/club-stream.php
+++ Website/club-stream.php
@@ -17,7 +17,7 @@
 
 <body>
   <nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center">
-    <a href="/" class="navbar-brand d-flex w-50 mr-auto"><img class="logo" src="Website/images/connect-me-logo2.png"/></a>
+    <a href="/" class="navbar-brand d-flex w-50 mr-auto"><img class="logo" src="images/connect-me-logo2.png"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,7 +31,7 @@
             </li>
             <li class="nav-item">
               <div class="dropdown">
-                <img class="navbar-profile-pic dropbtn" src="Website/images/blank-avatar-green.png" onclick="myFunction()">
+                <img class="navbar-profile-pic dropbtn" src="images/blank-avatar-green.png" onclick="myFunction()">
                 <div id="myDropdown" class="dropdown-content">
                   <a href="interest-quiz.php">Interest Quiz</a>
                   <a href="your-clubs.php">Your Clubs</a>
